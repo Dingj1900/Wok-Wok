1e898161b1204a1c9452cdfb247a0ec83ac01683
Dingj1900 <dingj106@gmail.com>
2025-07-10 18:33:05 -0400

    adding frontpage with navbar

--- Wok-Wok-App/src/App.vue
+++ Wok-Wok-App/src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import FrontPage from './components/FrontPage.vue';
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <div>
+    
     <FrontPage></FrontPage>
+
+    <NavBar></NavBar>
   </div>
 </template>
 
--- Wok-Wok-App/src/components/FrontPage.vue
+++ Wok-Wok-App/src/components/FrontPage.vue
@@ -1,8 +1,19 @@
 <template>
+  <div class = "home">
+    <div class = "gird-container">
+      <div>
+        
+      </div>
 
-    
+    </div>
+
+  </div>
 </template>
 
 <script>
 
 </script>
+
+<style scoped>
+
+</style>
